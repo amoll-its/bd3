91bd0b83aa08cd1348d94e2d8e2b82255dcd7598
amoll-its <amoll@its.com.uy>
2015-11-19 18:41:49 -0300

    n/a

---
+++ ObligatorioBD3/src/persistencia/fabrica/FabricaSQL.java
@@ -0,0 +1,22 @@
+package persistencia.fabrica;
+
+import persistencia.daos.DAODueños;
+import persistencia.daos.DAOMascotas;
+import persistencia.daos.IDAODueños;
+import persistencia.daos.IDAOMascotas;
+
+public class FabricaSQL implements FabricaAbstracta {
+
+	public FabricaSQL() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public IDAODueños crearDAODueños() {
+		return new DAODueños();
+	}
+
+	public IDAOMascotas crearDAOMascotas() {
+		return new DAOMascotas();
+	}	
+	
+}
---
+++ ObligatorioBD3/src/persistencia/fabrica/FabricaSerial.java
@@ -0,0 +1,9 @@
+package persistencia.fabrica;
+
+public class FabricaSerial implements FabricaAbstracta {
+
+	public FabricaSerial() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
